e3f2359f91bd09bc6a78cb4af171ff0d7453a751
Colin Walters <walters@verbum.org>
2019-09-24 21:08:38 +0000

    dracut: Add 31ignition-ostree module
    I'm working on supporting reprovisioning the rootfs with Ignition
    on OSTree based systems like Fedora CoreOS, and this will add
    a lot of tricky logic that relates the two.
    
    While we want to support Ignition independent of OSTree and vice
    versa, it will be a lot cleaner if we centralize the dracut
    logic in one place.
    
    This adds a `31ignition-ostree` module that imports the code
    from
    https://github.com/coreos/fedora-coreos-config/tree/092f680a5c75324e3efcd5e50374a2c1b5d4b057/overlay.d/05core/usr/lib/dracut/modules.d/40coreos-var
    
    Most notably, this is:
    
     1) A separate dracut module easily skippable
     2) The units use `ConditionKernelCommandline=ostree` so will be harmless even if included
    
    The units imported here also do this:
    
    ```
    ConditionPathExists=!/usr/sbin/coreos-mount-var
    ```
    
    So this change can be safely merged, built and shipped as an RPM,
    then we can drop the FCOS config bits and enable it later.
    
    Tested both ways (inert and enabled).
    
    And to clarify, the later ignition-ostree-rootfs stuff will land
    in this same dracut module.

---
+++ dracut/31ignition-ostree/module-setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+
+depends() {
+    echo ignition ostree
+}
+
+install_ignition_unit() {
+    unit=$1; shift
+    inst_simple "$moddir/$unit" "$systemdsystemunitdir/$unit"
+    ln_r "../$unit" "$systemdsystemunitdir/ignition-complete.target.requires/$unit"
+}
+
+install() {
+    inst_multiple \
+        systemd-sysusers \
+        systemd-tmpfiles
+
+    mkdir -p "$initdir/$systemdsystemunitdir/ignition-complete.target.requires"
+
+    install_ignition_unit ignition-ostree-mount-var.service
+    inst_script "$moddir/ignition-ostree-mount-var.sh" \
+        "/usr/sbin/ignition-ostree-mount-var"
+
+    install_ignition_unit ignition-ostree-populate-var.service
+    inst_script "$moddir/ignition-ostree-populate-var.sh" \
+        "/usr/sbin/ignition-ostree-populate-var"
+}
