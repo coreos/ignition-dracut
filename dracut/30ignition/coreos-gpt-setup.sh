0b1da4c8bef1b0b74897f99a98ece3ccb7a6595d
Tuan Hoang <tmhoang@linux.ibm.com>
2020-03-11 18:35:58 +0100

    dracut: update disk/by-label links for multipath devices

--- dracut/30ignition/coreos-gpt-setup.sh
+++ dracut/30ignition/coreos-gpt-setup.sh
@@ -6,6 +6,12 @@ set -euo pipefail
 
 UNINITIALIZED_GUID='00000000-0000-4000-a000-000000000001'
 
+# in case of using multipath devices, we need to make sure all
+# /dev/disk/by-label/ links are fully populated and updated.
+udevadm trigger --type=subsystems --action=add
+udevadm trigger --type=devices --action=add
+udevadm settle
+
 # PTUUID is the disk guid, PKANME is the parent kernel name
 eval $(lsblk --output PTUUID,PKNAME --pairs --paths --nodeps "$1")
 [ "$PTUUID" != "$UNINITIALIZED_GUID" ] && exit 0
