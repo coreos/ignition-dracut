a7fa24673fee6b4c08655d78d3001519322cf698
Ben Howard <ben.howard@redhat.com>
2020-05-04 14:55:32 -0600

    boot.mount: introduce boot.mount to prevent boot racing
    In situations with complicated disk setups (i.e. multipath) we need to
    control when boot is mounted until after the raw device is ready, not
    just there.
    
    Signed-off-by: Ben Howard <ben.howard@redhat.com>

--- dracut/30ignition/ignition-setup-user.sh
+++ dracut/30ignition/ignition-setup-user.sh
@@ -20,13 +20,6 @@ if command -v is-live-image >/dev/null && is-live-image; then
     copy_file_if_exists "/config.ign" "${destination}/user.ign"
 else
     # We will support a user embedded config in the boot partition
-    # under $bootmnt/ignition/config.ign. Note that we mount /boot
-    # but we don't unmount boot because we are run in a systemd unit
-    # with MountFlags=slave so it is unmounted for us.
-    bootmnt=/mnt/boot_partition
-    mkdir -p $bootmnt
-    # mount as read-only since we don't strictly need write access and we may be
-    # running alongside other code that also has it mounted ro
-    mount -o ro /dev/disk/by-label/boot $bootmnt
-    copy_file_if_exists "${bootmnt}/ignition/config.ign" "${destination}/user.ign"
+    # under $bootmnt/ignition/config.ign.
+    copy_file_if_exists "/boot/ignition/config.ign" "${destination}/user.ign"
 fi
