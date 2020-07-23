2733e1c7d0c4a234b7af8a31812ab9bacb080870
Jonathan Lebon <jonathan@jlebon.com>
2020-07-23 14:39:34 -0400

    99emergency-timeout/virtio-dump: wait before dumping
    When systemd activates us as a result of switching to
    `emergency.target`, journald might still be collecting and writing back
    messages from various services. If we run too early, we'll be missing
    out on those.
    
    As a hack, wait until the 'Journal stopped' message from journald is
    written before dumping the whole journal. Timeout at 30s.

--- dracut/99emergency-timeout/ignition-virtio-dump-journal.sh
+++ dracut/99emergency-timeout/ignition-virtio-dump-journal.sh
@@ -3,6 +3,13 @@ set -euo pipefail
 
 port=/dev/virtio-ports/com.coreos.ignition.journal
 if [ -e "${port}" ]; then
+    # wait for the journal to wind down
+    for i in {1..15}; do
+        if journalctl -q -u systemd-journald.service --grep 'Journal stopped'; then
+            break
+        fi
+        sleep 2
+    done
     journalctl -o json > "${port}"
     # And this signals end of stream
     echo '{}' > "${port}"
